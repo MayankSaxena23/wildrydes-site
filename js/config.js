7a61cd34bf3d463b6ba105d99252ca0eb8a0385e
MayankSaxena23 <96781341+MayankSaxena23@users.noreply.github.com>
2025-01-06 15:16:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_m7QjVnrII', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6q8omvatatm3tmev5oa25c9bma', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_WezrkSrwH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7j7okm9aidkbabdq57mr7thoga', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
