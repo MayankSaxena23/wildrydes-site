d9afda526997500e25657f823577604d83e56828
MayankSaxena23 <96781341+MayankSaxena23@users.noreply.github.com>
2025-01-06 14:56:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_m7QjVnrII', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6q8omvatatm3tmev5oa25c9bma', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-1
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
